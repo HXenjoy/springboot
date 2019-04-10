2f976a68fcfd9f1c31e74a26e9b0fb36ad3dc992
HXenjoy <1083147221@qq.com>
2019-04-10 17:32:44 +0800

    v0.1

--- src/main/java/cn/enjoy/Example.java
+++ src/main/java/cn/enjoy/Example.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Example {
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello World! +1";
     }
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Example.class, args);
